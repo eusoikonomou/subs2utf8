a7d4eb26c193e2e02d125db11f9084d7b9afd540
Efstathios Oikonomou <eusoikonomou@gmail.com>
2019-05-10 23:28:42 +0300

    Updates

---
+++ precache-manifest.ac02c04c7df488b2951a45483c009afc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb1807a73b66dc9b19085cb2e57b870",
+    "url": "/subs2utf8/index.html"
+  },
+  {
+    "revision": "abbb94645e3b76c6c982",
+    "url": "/subs2utf8/static/css/main.39aa499c.chunk.css"
+  },
+  {
+    "revision": "872a6d8796860069f152",
+    "url": "/subs2utf8/static/js/2.8f3e59aa.chunk.js"
+  },
+  {
+    "revision": "abbb94645e3b76c6c982",
+    "url": "/subs2utf8/static/js/main.80881cc9.chunk.js"
+  },
+  {
+    "revision": "708d190f890247d1b4fb",
+    "url": "/subs2utf8/static/js/runtime~main.9d662ee8.js"
+  }
+]);
